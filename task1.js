7731bc3f39503c48673ad05440967b243ff5659e
il-04 <147924370+il-04@users.noreply.github.com>
2025-01-08 19:26:01 +0530

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,3 +1,74 @@
-react = frontend(type and submit)
-express = backend
-mongoDB = database
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Webpage</title>
+    <style>
+        .box{
+        }
+        td{
+            padding-left: 5px;
+        }
+        header{
+            background-color: brown;
+        }
+        header h1{
+            margin: 0px;
+            text-align: center;
+            color: white;
+            font-size: 50px;
+        }
+        footer{
+            background-color: brown;
+        }
+        footer h1{
+            margin: 0px;
+            text-align: center;
+            color: white;
+            font-size: 50px;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>The avengers</h1>
+    </header>
+    <header style="background-color: aqua;">
+        <table>
+            <tr>
+                <td>The</td>
+                <td>avengers</td>
+                <td>are</td>
+            </tr>
+        </table>
+        <ul>
+            <li>Captain America</li>
+            <li>Iron Man</li>
+            <li>Thor</li>
+        </ul>
+    </header>
+    <br>
+    <table>
+        <tr>
+            <td>
+                <div class="box" style="border: 4px solid black; width: 500px; height: 305px;">
+                    <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a364ee04-6380-4393-a551-2199250f57bf/dbtspza-04318586-4f6e-4363-b1b9-b9f285b1d475.jpg/v1/fill/w_1024,h_626,q_75,strp/the_avengers___wallpaper_by_daviddv1202_dbtspza-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjI2IiwicGF0aCI6IlwvZlwvYTM2NGVlMDQtNjM4MC00MzkzLWE1NTEtMjE5OTI1MGY1N2JmXC9kYnRzcHphLTA0MzE4NTg2LTRmNmUtNDM2My1iMWI5LWI5ZjI4NWIxZDQ3NS5qcGciLCJ3aWR0aCI6Ijw9MTAyNCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.D1AUjxXBzPIp_-Wq458tK5qIbcHjfuIW0C7sBpyYNx4" alt="" style="width: 500px;">
+                    <video style="width: 500px;">
+                        <source src="" type="video">
+                    </video>
+                    <audio>
+                        <source src="" type="audio">
+                    </audio>
+                </div>
+            </td>
+            
+        </tr>
+    </table>
+    <br>
+    <footer>
+        <h1>BYE!</h1>
+    </footer>
+</body>
+</html>
+
